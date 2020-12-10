b880387b92975966731037fe252bfcc73243ad6e
xhmo <xhmo@mobvoi.com>
2020-12-10 15:57:13 +0800

    created project

---
+++ MOWidgetDemo/MOWidgetDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MOWidgetDemo
+//
+//  Created by 莫晓卉 on 2020/12/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+
+  }
+
+
+}
+
