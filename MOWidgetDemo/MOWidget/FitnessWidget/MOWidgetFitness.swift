e6c8a61a9b979d3c64a79780ad2aa29137e3eb22
mikimo <mikimo@tencent.com>
2021-11-26 20:07:08 +0800

    add deviceType choose

--- MOWidgetDemo/MOWidget/FitnessWidget/MOWidgetFitness.swift
+++ MOWidgetDemo/MOWidget/FitnessWidget/MOWidgetFitness.swift
@@ -9,66 +9,31 @@ import WidgetKit
 import SwiftUI
 import Intents
 
-//class Device: INObject {
-//  static let availableTicWatch = [
-//    Device(identifier:"01", display: "TicWatch"),
-//    Device(identifier:"02", display: "TicWatch Pro"),
-//    Device(identifier:"03", display: "TicWatch E3")
-//  ]
-//  static let availableTicPod = [
-//    Device(identifier:"01", display: "TicPod"),
-//    Device(identifier:"02", display: "TicPod Pro"),
-//    Device(identifier:"03", display: "TicPod Plus")
-//  ]
-//}
-//
-//class IntenHandler: INExtension, ConfigurationIntentHandling {
-//  func provideDeviceOptionsCollection(for intent: ConfigurationIntent, with completion: @escaping (INObjectCollection<Device>?, Error?) -> Void) {
-//    var availableDevices: [Device]
-//    switch intent.deviceType {
-//    case .ticWatch: availableDevices = MODevice.availableTicWatch
-//    case .ticPod: availableDevices = MODevice.availableTicPod
-//    default: availableDevices = MODevice.availableTicWatch
-//    }
-//
-//    let characters: [Device] = availableDevices.map { device in
-//      let gameCharacter = Device (
-//        identifier: device.identifier,
-//        display: device.displayString
-//      )
-//      return gameCharacter
-//    }
-//    // Create a collection with the array of characters.
-//    let collection = INObjectCollection(items: characters)
-//
-//    // Call the completion handler, passing the collection.
-//    completion(collection, nil)
-//  }
-//}
-
 struct Provider: IntentTimelineProvider {
-    typealias Intent = ConfigurationIntent
     
+    /// 占位视图
     func placeholder(in context: Context) -> MOSimpleEntry {
-        print("placeholder")
-        return MOSimpleEntry(date: Date(), configuration: ConfigurationIntent(), data: MOFitnessData())
+        let size: CGSize = context.displaySize;
+        print("placeholder \(size)")
+        return MOSimpleEntry(date: Date(), configuration: DeviceSelectionIntent(), data: MOFitnessData())
     }
     
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (MOSimpleEntry) -> ()) {
-        print("getSnapshot")
+    /// 快照：小组件库里的显示
+    func getSnapshot(for configuration: DeviceSelectionIntent, in context: Context, completion: @escaping (MOSimpleEntry) -> ()) {
+        let size: CGSize = context.displaySize;
+        print("getSnapshot \(size)")
         let entry = MOSimpleEntry(date: Date(), configuration: configuration, data: MOFitnessData())
         completion(entry)
     }
     
-    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<MOSimpleEntry>) -> ()) {
-        print("getTimeline")
-        print("getTimeline deviceType: \(configuration.deviceType.rawValue)")
-        print("getTimeline device: \(configuration.device?.identifier)")
+    /// 在这个方法内可以进行网络请求，拿到的数据保存在对应的entry中，调用completion之后会到刷新小组件
+    func getTimeline(for configuration: DeviceSelectionIntent, in context: Context, completion: @escaping (Timeline<MOSimpleEntry>) -> ()) {
+        let size: CGSize = context.displaySize;
+        print("getTimeline \(size)")
+        print("getTimeline deviceType: \(configuration.device.rawValue)")
+        
         var entries: [MOSimpleEntry] = []
         let currentDate = Date()
-        
-        //    let name = configuration.character.name
-        
         let entryDate = Calendar.current.date(byAdding: .minute, value: 1, to: currentDate)!
         MODataSouce.shared.fetchData { (data) in
             // 请求成功
@@ -90,7 +55,7 @@ struct MOData {
 
 struct MOSimpleEntry: TimelineEntry {
     let date: Date
-    let configuration: ConfigurationIntent
+    let configuration: DeviceSelectionIntent
     let data: MOFitnessData
 }
 
@@ -125,7 +90,7 @@ struct MOWidgetFitness: Widget {
     let kind: String = "MOWidgetFitness"
     
     var body: some WidgetConfiguration {
-        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+        IntentConfiguration(kind: kind, intent: DeviceSelectionIntent.self, provider: Provider()) { entry in
             MOWidgetEntryView(entry: entry)
         }
         .configurationDisplayName("widget.name")
@@ -138,11 +103,11 @@ struct MOFitnessWidget_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ForEach(["iPhone 12 mini", "iPhone 12", "iPhone 12 Pro Max"], id: \.self) { deviceName in
-                //        MOWidgetEntryView(entry: MOSimpleEntry(date: Date(), configuration: ConfigurationIntent(), data: MOFitnessData()))
-                //          .previewContext(WidgetPreviewContext(family: .systemSmall))
-                //          .previewDevice(PreviewDevice(rawValue: deviceName))
+                MOWidgetEntryView(entry: MOSimpleEntry(date: Date(), configuration: DeviceSelectionIntent(), data: MOFitnessData()))
+                  .previewContext(WidgetPreviewContext(family: .systemSmall)) // family
+                  .previewDevice(PreviewDevice(rawValue: deviceName)) // 显示的设备
                 
-                MOWidgetEntryView(entry: MOSimpleEntry(date: Date(), configuration: ConfigurationIntent(), data: MOFitnessData()))
+                MOWidgetEntryView(entry: MOSimpleEntry(date: Date(), configuration: DeviceSelectionIntent(), data: MOFitnessData()))
                     .previewContext(WidgetPreviewContext(family: .systemMedium))
                     .previewDevice(PreviewDevice(rawValue: deviceName))
             }
@@ -154,6 +119,6 @@ struct MOFitnessWidget_Previews: PreviewProvider {
 struct MOWigets: WidgetBundle {
     var body: some Widget {
         MOWidgetFitness()
-        MOWidgetSecond()
+//        MOWidgetSecond()
     }
 }
